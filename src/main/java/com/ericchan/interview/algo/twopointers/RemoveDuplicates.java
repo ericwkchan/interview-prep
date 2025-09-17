cb83ba6f5962b136a281dfabe57a1e79d0e73bbb
Eric Chan <ericchan@mac.myfiosgateway.com>
2025-09-17 18:41:59 -0400

    Solve remove duplicates problem with two pointers

---
+++ src/main/java/com/ericchan/interview/algo/twopointers/RemoveDuplicates.java
@@ -0,0 +1,33 @@
+package com.ericchan.interview.algo.twopointers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RemoveDuplicates {
+    public int removeDuplicates(List<Integer> nums) {
+        if (nums.isEmpty()) {
+            return 0;
+        }
+
+        int slow = 0;
+
+        for (int fast = 0; fast < nums.size(); fast++) {
+            if (!nums.get(fast).equals(nums.get(slow))) {
+                slow++;
+                nums.set(slow, nums.get(fast));
+            }
+        }
+
+        return slow + 1;
+        /**
+         * Review: So we have to assume this is sorted otherwise we can just use a hash set
+         * while iterating. And also, we can't use extra memory. So the way we do this is
+         * we have two pointers, one moving faster and one lagging behind. The one that
+         * always iterates, checks if the value is the same as the lagging pointer. If it
+         * changed at all, update the position of slow pointer to value of faster pointer
+         * and iterate the slow pointer. Keep going until end of the array and if you
+         * take the slow pointer + 1, that's the number of duplicates. we're not really
+         * removing duplicates which is silly to the name of this problem.
+         */
+    }
+}
---
+++ src/test/java/com/ericchan/interview/algo/arrays/twopointers/RemoveDuplicatesTest.java
@@ -0,0 +1,57 @@
+package com.ericchan.interview.algo.arrays.twopointers;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.ericchan.interview.algo.twopointers.RemoveDuplicates;
+import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class RemoveDuplicatesTest {
+
+    @Test
+    public void testExampleCase() {
+        RemoveDuplicates rd = new RemoveDuplicates();
+        List<Integer> nums = new ArrayList<>(Arrays.asList(0, 0, 1, 1, 1, 2, 2));
+
+        int resultLength = rd.removeDuplicates(nums);
+
+        assertEquals(3, resultLength);
+        assertEquals(Arrays.asList(0, 1, 2), nums.subList(0, resultLength));
+    }
+
+    @Test
+    public void testAllUnique() {
+        RemoveDuplicates rd = new RemoveDuplicates();
+        List<Integer> nums = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5));
+
+        int resultLength = rd.removeDuplicates(nums);
+
+        assertEquals(5, resultLength);
+        assertEquals(Arrays.asList(1, 2, 3, 4, 5), nums.subList(0, resultLength));
+    }
+
+    @Test
+    public void testSingleElement() {
+        RemoveDuplicates rd = new RemoveDuplicates();
+        List<Integer> nums = new ArrayList<>(Arrays.asList(42));
+
+        int resultLength = rd.removeDuplicates(nums);
+
+        assertEquals(1, resultLength);
+        assertEquals(Arrays.asList(42), nums.subList(0, resultLength));
+    }
+
+    @Test
+    public void testEmptyList() {
+        RemoveDuplicates rd = new RemoveDuplicates();
+        List<Integer> nums = new ArrayList<>();
+
+        int resultLength = rd.removeDuplicates(nums);
+
+        assertEquals(0, resultLength);
+        assertTrue(nums.isEmpty());
+    }
+}
+
