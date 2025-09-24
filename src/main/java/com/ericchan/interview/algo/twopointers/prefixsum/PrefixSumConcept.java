ec0e6d2d2d8ae17847d2bb11091b91ad6a91771f
Eric Chan <ericchan@mac.myfiosgateway.com>
2025-09-24 16:17:49 -0400

    Add suffix sum

--- src/main/java/com/ericchan/interview/algo/twopointers/prefixsum/PrefixSumConcept.java
+++ src/main/java/com/ericchan/interview/algo/twopointers/prefixsum/PrefixSumConcept.java
@@ -26,4 +26,14 @@ public class PrefixSumConcept {
     }
     return prefix;
   }
+
+  public static int[] suffixSum(int[] nums) {
+    int n = nums.length;
+    int[] suffix = new int[n];
+    suffix[n-1] = nums[n-1];
+    for (int i = n-2; i >= 0; i--) {
+      suffix[i] = nums[i] + suffix[i+1];
+    }
+    return suffix;
+  }
 }
